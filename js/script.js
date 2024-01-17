80d0f31268d0ad22a534e40fff29b359730f925f
¨Evelyn <ayelenecespedes@gmail.com>
2024-01-17 14:35:25 -0300

    carga inicial

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let nombreUsuario = prompt ("ingrese nombre del usuario")
+let edadUsuario = parseInt (prompt ("Ingrese edad del usuario"))
+
+
+
+alert ("bienvenido a Escabioso "+nombreUsuario)
+alert ("Sos mayor de 18 años podes ingresar")
+
+
+
+
+console.log (nombreUsuario)
+console.log (edadUsuario)
